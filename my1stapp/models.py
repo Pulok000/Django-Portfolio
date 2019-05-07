9e9dbe4f1057a9bf6c818d43210893664a769d7f
Pulok000 <pulokahmed07@gmail.com>
2019-05-07 16:19:19 +0600

    email lenth 255

--- my1stapp/models.py
+++ my1stapp/models.py
@@ -11,7 +11,7 @@ class student(models.Model):
 
 class contact(models.Model):
 
-    email=models.EmailField(max_length=258)
+    email=models.EmailField(max_length=255)
     subject=models.CharField(max_length=100)
     message=models.TextField()
 
